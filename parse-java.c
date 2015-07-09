13f4a715a559c73a35c5f420ad3537f3cbd56468
Chaos Shen <s1z2j3@gmail.com>
2015-07-09 23:06:34 +0800

    Add license.

--- parse-java.c
+++ parse-java.c
@@ -1,5 +1,22 @@
 /*
- * parse-java.c - Chaos Shen
+ * parse-java.c - parser of Java source code
+ *
+ * Copyright (C) 2015 Chaos Shen
+ *
+ * This file is part of parse-java.
+ *
+ * parse-java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * parse-java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with parse-java.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifdef _MSC_VER
